6a6c8a79dc2867de97690587591362b8a7dddc69
Shobha4-Goudar <shobhavgoudar4@gmail.com>
2025-11-21 23:36:02 +0530

    Create Movie entity class with attributes and methods
    This class represents a Movie entity with properties such as id, name, genre, image link, and rating, along with their respective getters and setters.

---
+++ Movie.java
@@ -0,0 +1,58 @@
+package moviecrud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String genre;
+	private String imagelink;
+	private double rating;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getImagelink() {
+		return imagelink;
+	}
+
+	public void setImagelink(String imagelink) {
+		this.imagelink = imagelink;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+}
